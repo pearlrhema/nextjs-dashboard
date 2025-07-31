eb3abbde60f70a7aa365ccaf72a1050c62df0b95
pearlrhema <pearlrhema8@gmail.com>
2025-07-31 20:13:07 +0100

    authentication to login route

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,9 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from "postgres";
 
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
 // Use Zod to update the expected types
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
@@ -117,4 +120,23 @@ export async function deleteInvoice(id: string) {
   // Unreachable code block
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
